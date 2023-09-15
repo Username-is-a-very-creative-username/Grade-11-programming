d72afbd2bfe03d963cd9de5795230b9d6713c7e8
Large Beard Dude <114947306+Username-is-a-very-creative-username@users.noreply.github.com>
2023-09-15 13:56:39 -0400

    Create Radius-of-a-circle.java

---
+++ Radius-of-a-circle.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    double Pie=3.14; 
+    double Circle1=5.56; 
+    double Circle2=42.121;
+    int Circle3=5; 
+    
+    
+    System.out.println("(5.56 cm) Radius of Circle1: " + (Pie * Circle1 * Circle1 + " Cm"));
+    System.out.println("(42.121 cm) Radius of Circle2: " + (Pie * Circle2 * Circle2 + " Cm"));
+    System.out.println("(5 cm) Radius of Circle3: " + (Pie * Circle3 * Circle3 + " Cm"));
+  
+  }
+}
