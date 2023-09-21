702b167d8bfdb85a5bea386185f014057461f988
Large Beard Dude <114947306+Username-is-a-very-creative-username@users.noreply.github.com>
2023-09-21 13:58:52 -0400

    Update Average-of-numbers.java

--- Average-of-numbers.java
+++ Average-of-numbers.java
@@ -1,10 +1,8 @@
 class Main {
   public static void main(String[] args) {
-    double score1 = 450.1;
-    double score2 = 88.8;
-    double score3 = 940.8; 
-    double score4 = 1684.90; 
-    double average = ((score1 + score2 + score3 + score4) / 4); 
+    double score1 = 88.8;
+    double score2 = 1684.90; 
+    double average = ((score1 + score2) / 2); 
     System.out.println("The Average is " + average);
   }
-} 
+}
