85c0c7fffb5efde20d2ff6e3d26f49c9a7bc963b
Large Beard Dude <114947306+Username-is-a-very-creative-username@users.noreply.github.com>
2023-09-21 13:55:31 -0400

    Create Average-of-numbers.java

---
+++ Average-of-numbers.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    double score1 = 450.1;
+    double score2 = 88.8;
+    double score3 = 940.8; 
+    double score4 = 1684.90; 
+    double average = ((score1 + score2 + score3 + score4) / 4); 
+    System.out.println("The Average is " + average);
+  }
+} 
