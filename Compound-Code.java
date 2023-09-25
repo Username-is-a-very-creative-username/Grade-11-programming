7a39568c0a793c172fd3c6047c9342f9822f4ef8
Large Beard Dude <114947306+Username-is-a-very-creative-username@users.noreply.github.com>
2023-09-25 13:29:51 -0400

    Create Compound-Code.java

---
+++ Compound-Code.java
@@ -0,0 +1,10 @@
+// Compound Assignment Operator!
+class Main {
+  public static void main(String[] args) {
+   int D = 7;
+   System.out.println("Addition: " + (D+=1));
+   System.out.println("Subtraction: " + (D-=1));
+   System.out.println("Muliplication: " + (D*=2));
+   System.out.println("Division: " + (D/=2));
+  }
+} 
