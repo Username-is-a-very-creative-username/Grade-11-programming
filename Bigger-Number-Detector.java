f4f44f106f4755cd4709ebf448ac2d359b601759
Large Beard Dude <114947306+Username-is-a-very-creative-username@users.noreply.github.com>
2023-09-27 13:19:06 -0400

    Create Bigger-Number-Detector.java

---
+++ Bigger-Number-Detector.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+    int p=7, v=3; 
+    if (p>v)
+    {
+    System.out.println("p is bigger");
+   }
+    else
+      {
+      System.out.println("v is bigger");
+      }
+  }
+  } 
