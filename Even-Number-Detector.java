d96aacce22194036c2911b4c01ee94e37f594a54
Large Beard Dude <114947306+Username-is-a-very-creative-username@users.noreply.github.com>
2023-09-27 13:17:56 -0400

    Create Even-Number-Detector.java

---
+++ Even-Number-Detector.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+    int Number=55;
+    if (Number%2==0)
+    {
+    System.out.println("This Number is Even");
+   }
+    else
+      {
+      System.out.println("This Number is NOT Even");
+      }
+  }
+} 
