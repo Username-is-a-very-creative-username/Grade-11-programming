9a2da7efb7cee629abd77df6b5eaa5336ccac525
Large Beard Dude <114947306+Username-is-a-very-creative-username@users.noreply.github.com>
2023-09-29 19:02:09 -0400

    Radius-of-a-circle.java

--- Radius-of-a-circle.java
+++ Radius-of-a-circle.java
@@ -4,11 +4,11 @@ class Main {
     final double Circle1=5.56; 
     final double Circle2=42.121;
     int Circle3=5; 
-    
-    
+    /* You can calculate multiple integers in a single line as well although when combining it with "", other () are required to separate them. 
+    */  
     System.out.println("(5.56 cm) Radius of Circle1: " + (Pie * Circle1 * Circle1 + " Cm"));
     System.out.println("(42.121 cm) Radius of Circle2: " + (Pie * Circle2 * Circle2 + " Cm"));
-    System.out.println("(5 cm) Radius of Circle3: " + (Pie * Circle3 * Circle3 + " Cm"));
-  
+    System.out.println("(5 cm) Radius of Circle3: " + (Pie * Circle3 * Circle3 + " Cm")); 
+    
   }
 }
