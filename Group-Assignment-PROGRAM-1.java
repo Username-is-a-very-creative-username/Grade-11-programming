ee07e79d2d19f3a0922b2b07cde722238beef8af
Large Beard Dude <114947306+Username-is-a-very-creative-username@users.noreply.github.com>
2023-10-10 13:54:23 -0400

    Create Group-Assignment-PROGRAM-1.java

---
+++ Group-Assignment-PROGRAM-1.java
@@ -0,0 +1,24 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("oOOO()");
+    System.out.println(" /  _)");
+    System.out.println(" |  (");
+    System.out.println(" \\__)");
+
+    System.out.println("      ()OOOo");
+    System.out.println("       (_  \\");
+    System.out.println("        )  |");
+    System.out.println("        (__/");
+
+    System.out.println("oOOO()");
+    System.out.println(" /  _)");
+    System.out.println(" |  (");
+    System.out.println(" \\__)");
+
+    System.out.println("      ()OOOo");
+    System.out.println("       (_  \\");
+    System.out.println("        )  |");
+    System.out.println("        (__/");
+   
+  }
+}
